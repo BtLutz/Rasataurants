6871391a0e661da0b6cab4eb7f35dfe53e8d67b6
BtLutz <brian.lutz9@icloud.com>
2018-11-29 15:36:28 -0800

    Initial Commit

---
+++ Rasataurants/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rasataurants
+//
+//  Created by Brian Lutz on 11/29/18.
+//  Copyright © 2018 Gecko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
